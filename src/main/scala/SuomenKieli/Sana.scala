7cad38388937d2f4ca6d6e8fff293b11089956d8
sdmueller1 <s.mueller0311@gmail.com>
2023-07-22 18:00:58 -0500

    adding shit

--- src/main/scala/SuomenKieli/Sana.scala
+++ src/main/scala/SuomenKieli/Sana.scala
@@ -1,11 +1,42 @@
 package SuomenKieli
 import SanaOps.*
+
+import scala.util.matching.Regex
 // Matches type A words and captures the relevant syllable for gradation
-val typeAregex = s".*([$vowels][$consonants]{1,2}[aäoöuyi])$$".r
+val typeAregex = s"(.*)([$vowels][$consonants]{1,2}[aäoöuyi])$$".r
 // Matches type A words and captures the relevant syllable for gradation
-val typeBregex = s".*([$consonants]{1,2}[$vowels])[$consonants]$$".r
+val typeBregex = s"(.*)([$vowels][$consonants]{1,2}[$vowels])([$consonants])$$".r
 // Type C words never undergo gradation, so they are separated
 val typeCregex = s".*(nen|[uyoö]s)$$".r
+val strongMap: Map[Regex, String] = Map(
+  "[^skht]k[^skht]".r -> "kk",
+  "[^skht]p[^skht]".r -> "pp",
+  "[^skht]t[^skht]".r -> "tt",
+  "[^skht]d[^skht]".r -> "t",
+  "[uy]v[uy]".r -> "uku",
+  "[^skht]v[^skht]".r -> "p",
+  ".nn.".r -> "nt",
+  ".ng.".r -> "nk",
+  ".mm.".r -> "mp",
+  ".ll.".r -> "lt",
+  ".rr.".r -> "rt",
+  "lje".r -> "lke",
+  "rje".r -> "rke",
+  "hke".r -> "hke"
+)
+def matchregexp(r:Regex,)
+def gradate(s:String):String = s match {
+  case typeCregex(c) => s
+  case typeBregex(s,m,e) =>
+    println(s+"|"+m+"|"+e)
+    strongMap.foreach((a,b)=>println(a.toString() + " " + m.matches(a.toString())))
+    s+strongMap.collectFirst((k,v) => if(k.matches(m))Some(v)else None).getOrElse(m)+e
+  case typeAregex(s,m) =>
+    println(s+"|"+m)
+    s
+    //s+weakMap.collectFirst((k,v)=>if(k.matches(m))v).getOrElse(m)
+  case _ => s
+}
 class Sana(nominatiivi:String):
   val self:TypedWord = nominatiivi match {
     case typeCregex(s) => typeCWord(nominatiivi,s)
@@ -13,47 +44,52 @@ class Sana(nominatiivi:String):
     case typeAregex(_) => typeAWord(nominatiivi)
     case _ => typeAWord(nominatiivi)
   }
+  val weakStem = self.weakStem
+  val strongStem = self.strongStem
 
   class typeAWord(nominative:String) extends TypedWord(nominative):
-    println("A")
+    var weakStem = gradate(nominative)
+    var strongStem = nominative
 
 
   class typeBWord(nominative: String) extends TypedWord(nominative):
-    println("B")
+    var weakStem = nominative
+    var strongStem = gradate(nominative)
 
 
   class typeCWord(nominative:String,ending:String) extends TypedWord(nominative):
-    println("C")
+    var weakStem = nominative
+    var strongStem = nominative
 
   trait TypedWord(nominative:String):
-    val weakStem:String
-    val strongStem:String
-    def genitive:String
-    def essive:String
-    def partitive:String
-    def translative:String
-    def inessive:String
-    def elative:String
-    def illative:String
-    def adessive:String
-    def ablative:String
-    def allative:String
-    def abessive:String
-    def comitive:String
-    def instructive:String
-    def pluralGenitive: String
-    def pluralEssive: String
-    def pluralPartitive: String
-    def pluralTranslative: String
-    def pluralInessive: String
-    def pluralElative: String
-    def pluralIllative: String
-    def pluralAdessive: String
-    def pluralAblative: String
-    def pluralAllative: String
-    def pluralAbessive: String
-    def pluralComitive: String
-    def pluralInstructive: String
+    var weakStem:String
+    var strongStem:String
+//    def genitive:String
+//    def essive:String
+//    def partitive:String
+//    def translative:String
+//    def inessive:String
+//    def elative:String
+//    def illative:String
+//    def adessive:String
+//    def ablative:String
+//    def allative:String
+//    def abessive:String
+//    def comitive:String
+//    def instructive:String
+//    def pluralGenitive: String
+//    def pluralEssive: String
+//    def pluralPartitive: String
+//    def pluralTranslative: String
+//    def pluralInessive: String
+//    def pluralElative: String
+//    def pluralIllative: String
+//    def pluralAdessive: String
+//    def pluralAblative: String
+//    def pluralAllative: String
+//    def pluralAbessive: String
+//    def pluralComitive: String
+//    def pluralInstructive: String
 class Adjektiivi(text:String) extends Sana(text):
   text
 
--- src/test/scala/KieliOpsTest.scala
+++ src/test/scala/KieliOpsTest.scala
@@ -5,7 +5,7 @@ import scala.util.matching.Regex
   val words = FileIO.readFile("src/wordscores.csv").words.map(_.word.text).filterNot(_.contains(" "))
   val consmap = words.map(_.map(c => if vowels.contains(c) then 'V' else 'C'))
   val freg = "(C{0,2}V+C{1,2}V?)".r
-  consmap.map {n => n match
+  consmap.map {
     case freg(s) => s
     case _ => "BBBB"
   }.toSet.foreach(println)
@@ -15,18 +15,20 @@ import scala.util.matching.Regex
   //  case _ => println())
 
 val strongMap: Map[Regex, String] = Map(
-  "[^s]?k".r -> "kk",
-  "[^s]?p".r -> "pp",
-  "[^s]?t".r -> "tt",
-  "nn".r -> "nt",
-  "ng".r -> "nk",
-  "mm".r -> "mp",
-  "ll".r -> "lt",
-  "rr".r -> "rt",
-  "d".r -> "t",
-  "v".r -> "p",
+  "[^skht]k[^skht]".r -> "kk",
+  "[^skht]p[^skht]".r -> "pp",
+  "[^skht]t[^skht]".r -> "tt",
+  "[^skht]d[^skht]".r -> "t",
+  "[uy]v[uy]".r -> "uku",
+  "[^skht]v[^skht]".r -> "p",
+  ".nn.".r -> "nt",
+  ".ng.".r -> "nk",
+  ".mm.".r -> "mp",
+  ".ll.".r -> "lt",
+  ".rr.".r -> "rt",
   "lje".r -> "lke",
-  "rje".r -> "rke"
+  "rje".r -> "rke",
+  "hke".r -> "hke"
 )
 def strengthen(str:String) = str match
   //start,middle,end
@@ -34,7 +36,8 @@ def strengthen(str:String) = str match
     s+strongMap.collectFirst((k,v)=>if(k.matches(m))v).getOrElse(m)+e
 
 @main def OpsTest():Unit =
-  val s = Sana("tyttanesdfsdn")
-
-
+  //val s = Sana("hillas")
+  //println(s.strongStem)
+  val test = strongMap.foldRight((a:Regex=>String,b:Regex=>String)=>a.)
+  println(test)
 
